da9b048d9c5269d429643eb451f5201c683d1dab
arifjagad <arifjagad34@gmail.com>
2024-03-15 18:18:36 +0700

    Update: Menambahkan json slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,17 +1,19 @@
-'use client';
+import React from 'react';
+import { Carousel } from 'flowbite-react';
+import jsonData from '../data/slide.json'; // Impor data JSON
 
-import {Carousel} from 'flowbite-react';
+const Slider = () => {
+    const sliderImages = jsonData[0].sliderHome; // Ambil array gambar dari data JSON
 
-export default function Slider() {
     return (
         <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
-            <Carousel slideInterval={5000}>
-                <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg" alt="..."/>
-                <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" alt="..."/>
-                <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" alt="..."/>
-                <img src="https://flowbite.com/docs/images/carousel/carousel-4.svg" alt="..."/>
-                <img src="https://flowbite.com/docs/images/carousel/carousel-5.svg" alt="..."/>
+            <Carousel slideInterval={5000} className="rounded-3xl">
+                {sliderImages.map((image, index) => (
+                    <img key={index} src={`/images/${image}`} alt={`Slider ${index + 1}`} />
+                ))}
             </Carousel>
         </div>
     );
 }
+
+export default Slider;
